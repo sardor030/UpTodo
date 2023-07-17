3bcf3bd0c1aae13eb8e07f4217b9a4cbcfb46941
ruzibekov <ruzibekov44@gmail.com>
2023-07-17 16:05:40 +0500

    add: background color, styles for toolbar, main screen, imageButton

---
+++ app/src/main/java/com/example/uptodo/screens/settings/SettingsFragment.kt
@@ -0,0 +1,7 @@
+package com.example.uptodo.screens.settings
+
+import androidx.fragment.app.Fragment
+import com.example.uptodo.R
+
+class SettingsFragment : Fragment(R.layout.fragment_settings) {
+}
